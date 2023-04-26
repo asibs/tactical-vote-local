83943ac4bd96fbd36d8fdb9e70c0b7cd8eb7922c
asibs <sibsvandal@hotmail.com>
2023-04-26 18:41:50 +0100

    Add favicons

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -38,7 +38,10 @@ export default function Layout({ children }: Props) {
       <Head>
         <title>Stop The Tories .Vote</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-        <link rel="icon" href="/favicon.ico" /> {/* TODO: Replace if/when we get a proper favicon! */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <div style={{ background: "var(--bs-gray-100)" }}>
