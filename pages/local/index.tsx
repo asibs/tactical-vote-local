40f15836fe47caa919a876b6b0d1f57a7b343b0d
asibs <sibsvandal@hotmail.com>
2023-04-30 11:21:01 +0100

    Update to latest version of CSV

--- pages/local/index.tsx
+++ pages/local/index.tsx
@@ -8,7 +8,7 @@ import Layout from '../../components/Layout'
 import Header from '../../components/Header'
 import ElectionLookup from '../../components/ElectionLookup/ElectionLookup'
 
-const ELECTION_DATA_CSV = 'data/local-tactical-2023-05-04-v6.csv'
+const ELECTION_DATA_CSV = 'data/local-tactical-2023-05-04-v7.csv'
 
 interface Props {
   councils: {
